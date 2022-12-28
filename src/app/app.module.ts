917f3b52c5011e772d959075b390b5214a97610a
Greezaaa <55212034+Greezaaa@users.noreply.github.com>
2022-12-28 03:10:11 +0100

    paginator applyed (need translation to spanish)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
+//main core
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header.component';
-import { FormSearchComponent } from './components/form-search/form-search.component';
-import { IssueListComponent } from './components/issue-list/issue-list.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { PaginatorComponent } from './components/paginator/paginator.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers } from './states/app.states';
-// import { reposReducer } from './states/reducers/repos.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//redux
+import { reducers } from './states/app.states';
+// import { reposReducer } from './states/reducers/repos.reducer'; // change to reducers if more then one store
+
+//material UI
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatListModule } from '@angular/material/list';
+
+//components
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormSearchComponent } from './components/form-search/form-search.component';
+import { PaginatePipe } from './pipes/paginate.pipe';
 
 @NgModule({
   declarations: [
@@ -21,18 +28,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HeaderComponent,
     FormSearchComponent,
     IssueListComponent,
-    PaginatorComponent,
+    PaginatePipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatPaginatorModule,
+    MatListModule,
     StoreModule.forRoot(reducers),
-    // StoreModule.forRoot({ repos: reposReducer }),
+    // StoreModule.forRoot({ repos: reposReducer }), // capply this if more then one store
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-    BrowserAnimationsModule  ],
-  providers: [],
+    BrowserAnimationsModule],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -6,6 +6,7 @@ import { Repo } from '../../intefaces/issues.inteface';
 import { Store } from '@ngrx/store';
 import { setRepos } from 'src/app/states/actions/setRepos.action';
 import { Observable } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-issue-list',
   templateUrl: './issue-list.component.html',
@@ -15,40 +16,48 @@ export class IssueListComponent {
 
   // issues:Array<Object> ; 
   issues: Array<Issue>;
-  repos$:Observable<Array<Repo>>;
+  repos$: Observable<Array<Repo>>;
+  page_size: number = 5;
+  page_number: number = 1;
+  totalRepos: number = 0;
+  pageSizeOptions: number[] = [3, 5, 10, 25];
+  handlePage(e: PageEvent){
+    this.page_size = e.pageSize
+    this.page_number = e.pageIndex + 1
+  }
   constructor(
     private ghReposIssues: GHIssuesService,
     private ghRepos: GHRepos,
-    private ghReposStore: Store<{ reposState: Array<Repo> }>
+    private ghReposStore: Store<{ reposState: Array<Repo> }>,
   ) {
     this.issues = [];
     this.repos$ = ghReposStore.select('reposState');
 
   }
-//TODO: SACAR DE AQUI y poner en componente aparte!!!
+  //TODO: SACAR DE AQUI y poner en componente aparte!!!
   getIssues() {
     this.ghReposIssues.getData().subscribe((data) => {
       console.log(data.items)
       this.issues = data.items;
     })
   }
-// ///////////////////////////////////////////
+  // ///////////////////////////////////////////
 
 
   //TODO: Separar esta accion en diferente componente
   search(value: string) {
     console.log(value);
-    setTimeout(() => { 
-
-      if (value && value.length > 3) {
-        //TODO:
+    setTimeout(() => {
+      if (value && value.length > 5) {
         this.ghRepos.getData(value).subscribe((data) => {
           this.ghReposStore.dispatch(setRepos({ repos: data.items }));
         })
       }
     }, 500)
-    
+    this.repos$.subscribe(repos => {
+      this.totalRepos = repos.length;
+    });
   }
 
-// ngOnItin(){}
+  // ngOnItin(){}
 }
