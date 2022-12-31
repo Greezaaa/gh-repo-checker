fb91223635181291b8b6ab38c92f66edb673b5dc
Greezaaa <55212034+Greezaaa@users.noreply.github.com>
2022-12-31 05:54:09 +0100

    cleaned redux stores

--- src/app/reduxStates/actions/searchRepos.action.ts
+++
@@ -1,6 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { Repo } from 'src/app/intefaces/searchedRepos.inteface';
-
-export const searchRepos = createAction(
-  '[Repos] Set Repos',props<{ repositorios: Array<Repo> }>()
-  );
--- src/app/reduxStates/app.states.ts
+++ src/app/reduxStates/app.states.ts
@@ -1,9 +1,4 @@
-import { reposReducer } from './reducers/repos.reducer'; 
 import { searchedValueReducer } from './reducers/searchedValue.reducer';
-
-
 export const reducers={
-
-    reposState:reposReducer,
     searchState: searchedValueReducer
 }
