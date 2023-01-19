56db2a1614b395d4385a65bd668a8898429bd14d
Greezaaa <55212034+Greezaaa@users.noreply.github.com>
2023-01-20 00:16:31 +0100

    changed regex validator to admit '.' and '_'

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ export const ISSUES_PER_PAGE = 30
 export const ROOT_API_URL = 'https://api.github.com/'
 
 export function getRepoDataFromUrl (url: string): { owner: string, repo: string } | null {
-  const regex = /^https:\/\/github\.com\/(?<owner>[\w-]+)\/(?<repo>[\w-]+)\/?$/
+  const regex = /^https:\/\/github\.com\/(?<owner>[\w._-]+)\/(?<repo>[\w._-]+)\/?$/
   const match = url.match(regex)
 
   if ((match == null) || (match.groups == null)) return null
